baad0032851f3ac81fed956024075cd681a46c87
Max Phillips <max@maxtop.local>
2022-10-01 20:12:18 +1000

    Some more experimenting

--- pages/experiments/rolling.tsx
+++ pages/experiments/rolling.tsx
@@ -1,22 +1,77 @@
+import { Debug, Physics, usePlane, useSphere } from "@react-three/cannon";
+import { OrbitControls, TransformControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { useEffect, useRef } from "react";
+import type { Mesh } from "three";
 import UtilitiesPanel from "../../components/UtilitiesPanel";
 
-const Ground = () => (
-  <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.5, 0]}>
-    <planeBufferGeometry attach="geometry" args={[100, 100]} />
-    <meshStandardMaterial attach="material" color="red" />
-  </mesh>
-);
+const Ground = () => {
+  const [ref, api] = usePlane(
+    () => ({
+      rotation: [-Math.PI / 2, 0, 0],
+    }),
+    useRef<Mesh>(null!)
+  );
+
+  useEffect(() => {
+    const handleGyroscope = (event: DeviceOrientationEvent) => {
+      // Convert alpha, beta, gamma to x, y, z with a Y up coordinate system
+      const { alpha, beta, gamma } = event;
+      const x = (alpha || 0) * (Math.PI / 180);
+      const y = (beta || 0) * (Math.PI / 180);
+      const z = (gamma || 0) * (Math.PI / 180);
+
+      // Apply the rotation to the plane
+      api.rotation.set(x, y, z);
+    };
+
+    // Listen for changes in device orientation
+    window.addEventListener("deviceorientation", handleGyroscope);
+
+    // Stop listening for changes in device orientation
+    return () => {
+      window.removeEventListener("deviceorientation", handleGyroscope);
+    };
+  }, [api.rotation]);
+
+  return (
+    <mesh receiveShadow ref={ref}>
+      <planeBufferGeometry attach="geometry" args={[100, 100]} />
+      <meshStandardMaterial attach="material" color="white" transparent />
+    </mesh>
+  );
+};
+
+function Cube() {
+  const [ref] = useSphere(
+    () => ({
+      mass: 100,
+      position: [0, 5, 0],
+    }),
+    useRef<Mesh>(null!)
+  );
+  return (
+    <mesh ref={ref}>
+      <sphereGeometry />
+      <meshStandardMaterial color="hotpink" />
+    </mesh>
+  );
+}
 
 const Rolling = () => {
   return (
-    <Canvas
-      onCreated={(state) => state.camera.up.set(0, 0, 1)}
-      camera={{ position: [0, 100, 0], fov: 50 }}
-    >
-      <ambientLight />
-      <UtilitiesPanel />
-      <Ground />
+    <Canvas camera={{ position: [0, 10, 0] }}>
+      <Physics>
+        <Debug color="black" scale={1.1}>
+          <ambientLight />
+          <OrbitControls />
+          <UtilitiesPanel />
+          <TransformControls mode="rotate">
+            <Ground />
+          </TransformControls>
+          <Cube />
+        </Debug>
+      </Physics>
     </Canvas>
   );
 };
